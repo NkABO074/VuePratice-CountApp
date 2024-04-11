6d156b8f38bd9d2a4ae1b8576a6bff287625b1fd
Nkwe <nkabo074@gmail.com>
2024-04-11 12:46:46 +0100

    add JS for the count logic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
-<!-- the template  -->
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+</script>
+
+<!-- the template: IT's needed to run properly -->
 <template>
-  <div>
-    <h4>The Current count is ...</h4>
-    <h1>0</h1>
-    <button>-</button>
-    <button>+</button>
-  </div>
+  <main>
+    <div>
+      <h4>The Current count is ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  /* background-color: aliceblue; */
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
